a86d6112c3981bbfa42af6a9a9c5c8e1568c866b
erwan-ash <60441163+erwan-ash@users.noreply.github.com>
2021-03-23 14:01:46 +0100

    Updater vitesse et zizag ok

---
+++ Touhou ECS/Assets/ERWAN/MovementUpdaterEnemy1.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUpdaterEnemy1 : IUpdater
+{
+    public void SystemUpdate()
+    {
+        var playerAccessor = TAccessor<SpeedModule>.Instance();
+
+        foreach (var module in playerAccessor.GetAllModules())
+        {
+            var entity = module.gameObject;
+            var currentMod = playerAccessor.TryGetModule(entity);
+
+            if (currentMod != null)
+            {
+                entity.transform.position = entity.transform.forward * Time.deltaTime * currentMod.Speed;
+            }
+        }
+    }
+}
